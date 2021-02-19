fa98a2c8071e4b6fc26ceba8852b2a14a9ae8fb4
Mingaleev <art.mingaleev@yandex.ru>
2021-02-19 10:25:33 +0300

    Initial commit

---
+++ app/src/main/java/com/minga/android_note/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minga.android_note;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
